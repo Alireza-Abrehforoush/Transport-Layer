6ba3510975ada153a739253a0a22a74c8ca58369
Alireza Abrehforoush <wwe_alireza.abc2015@yahoo.com>
2021-05-28 07:31:52 -0700

    contains lots of errors

--- source.py
+++ source.py
@@ -7,27 +7,57 @@ import subprocess
 
 
 def main():
+    snd_x_axis = [0]
+    snd_y_axis = [0]
+    rcv_x_axis = [0]
+    rcv_y_axis = [0]
+    snd_period = 0
+    rcv_period = 0
     ip = "127.0.0.1"
     interval = 0.2
-    port = 4040
+    port = 4046
     server_process = subprocess.Popen(f'iperf3 -s -p {port} -i {interval}', encoding = 'utf-8', stdout = subprocess.PIPE, shell = True)
     client_process = subprocess.Popen(f'iperf3 -c {ip} -p {port} -i {interval}', encoding = 'utf-8', stdout = subprocess.PIPE, shell = True)
     time.sleep(15)
     while True:
         client_output = client_process.stdout.readline()
-        print(client_output)
-        print("#" * 50)
+        print(client_output)########################################
+        c_index = 0
+        client_bitrate_str = ''
+        if client_output.find("bits/sec") != -1:
+            c_index = client_output.find("bits/sec") + -3
+            while client_output[c_index] != ' ':
+                client_bitrate_str = client_output[c_index] + client_bitrate_str
+                c_index -= 1
+            snd_x_axis.append(snd_period + interval)
+            print('$' + client_bitrate_str + '$')
+            snd_y_axis.append(float(client_bitrate_str)/10 ** 6)
+            snd_period += interval
         if client_output == '':
             break
     while True:
         server_output = server_process.stdout.readline()
-        print(server_output)
-        print("@" * 50)
-        if server_output == '':
+        print(server_output)########################################
+        s_index = 0
+        server_bitrate_str = ''
+        if server_output.find("bits/sec") != -1:
+            s_index = server_output.find("bits/sec") - 3
+            while server_output[s_index] != ' ':
+                server_bitrate_str = server_output[s_index] + server_bitrate_str
+                s_index -= 1
+            
+            rcv_x_axis.append(rcv_period + interval)
+            print('#' + server_bitrate_str + '#')
+            rcv_y_axis.append(float(server_bitrate_str)/10 ** 6)
+            rcv_period += interval
+        if 'receiver' in server_output:
             break
             
+    plt.plot(snd_x_axis, snd_y_axis, color = 'red')
+    plt.plot(rcv_x_axis, rcv_y_axis, color = 'blue')
+    plt.show()
 
-    print("#################")
+
     capturer = pyshark.LiveCapture(interface = 'lo', display_filter = 'tcp.srcport == 4040 || tcp.dstport == 4040')
     timeout = 10
     start = time.time()
@@ -35,54 +65,54 @@ def main():
     received_packet_counter = 0
     dic_of_received_packets = {}
     
-    time_measure = 0.01
-    previous = 0
-    difference = 0
-    counter = 0
-    x_axis = [0]
+    # time_measure = 0.01
+    # previous = 0
+    # difference = 0
+    # counter = 0
+    # x_axis = [0]
 
 
 
-    snd_sum_of_lenghts = 0
-    snd_period = 0
-    snd_x_axis = [0]
-    snd_y_axis = [0]
+    # snd_sum_of_lenghts = 0
+    # snd_period = 0
+
 
 #    rcv_time_measure = 0.01
 #    rcv_previous = 0
 #    rcv_difference = 0
 
-    rcv_sum_of_lenghts = 0
-    rcv_period = 0
-    rcv_x_axis = [0]
-    rcv_y_axis = [0]
+    # rcv_sum_of_lenghts = 0
+    # rcv_period = 0
+
+
+
 #calculating Number of recieved packets and Number of retransmited packets
     for pkt in capturer.sniff_continuously():
         current_seq_number = pkt[pkt.transport_layer].seq_raw
         current_src_port = pkt[pkt.transport_layer].srcport
 
-        snd_bitrate = 0
-        difference = float(pkt.sniff_timestamp) - start
-        if int(current_src_port) == 4040:
-            snd_sum_of_lenghts += float(pkt.length)
-            snd_period += difference - previous
-            if snd_period > time_measure:
-                snd_bitrate = (snd_sum_of_lenghts/snd_period)
-                snd_x_axis.append(snd_period + x_axis[counter])
-                snd_y_axis.append(snd_bitrate/(10 ** 6))
-                snd_period = 0
-                snd_sum_of_lenghts = 0
-                counter += 1
-        else:
-            rcv_sum_of_lenghts += float(pkt.length)
-            rcv_period += difference - previous
-            if rcv_period > time_measure:
-                rcv_bitrate = (rcv_sum_of_lenghts/rcv_period)
-                rcv_x_axis.append(rcv_period + x_axis[counter])
-                rcv_y_axis.append(rcv_bitrate/(10 ** 6))
-                rcv_period = 0
-                rcv_sum_of_lenghts = 0
-                counter += 1
+        # snd_bitrate = 0
+        # difference = float(pkt.sniff_timestamp) - start
+        # if int(current_src_port) == 4040:
+        #     snd_sum_of_lenghts += float(pkt.length)
+        #     snd_period += difference - previous
+        #     if snd_period > time_measure:
+        #         snd_bitrate = (snd_sum_of_lenghts/snd_period)
+        #         snd_x_axis.append(snd_period + x_axis[counter])
+        #         snd_y_axis.append(snd_bitrate/(10 ** 6))
+        #         snd_period = 0
+        #         snd_sum_of_lenghts = 0
+        #         counter += 1
+        # else:
+        #     rcv_sum_of_lenghts += float(pkt.length)
+        #     rcv_period += difference - previous
+        #     if rcv_period > time_measure:
+        #         rcv_bitrate = (rcv_sum_of_lenghts/rcv_period)
+        #         rcv_x_axis.append(rcv_period + x_axis[counter])
+        #         rcv_y_axis.append(rcv_bitrate/(10 ** 6))
+        #         rcv_period = 0
+        #         rcv_sum_of_lenghts = 0
+        #         counter += 1
         
         if time.time() - start > timeout:
             break
@@ -96,12 +126,10 @@ def main():
             else:
                 dic_of_received_packets[current_seq_number] = 1
     
-        previous = difference
-        x_axis.append(difference)
+        # previous = difference
+        # x_axis.append(difference)
 
-    plt.plot(snd_x_axis, snd_y_axis, color = 'red')
-    plt.plot(rcv_x_axis, rcv_y_axis, color = 'blue')
-    plt.show()
+
     
 
 
